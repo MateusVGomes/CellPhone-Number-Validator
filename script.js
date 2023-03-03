53dbb382fb1aef12c5f665d1b1e04a330cdf81ad
Mateus V. Gomes <63268362+MateusVGomes@users.noreply.github.com>
2023-03-03 12:11:25 -0300

    Alteração da lógica
    Antes a lógica tentava tratar de cada caso especial caso a caso, a lógica foi alterada para regex, a regex foi separada em pequenos pedaços e depois combinado para tornar mais clara a leitura da regex.

--- script.js
+++ script.js
@@ -1,56 +1,10 @@
 function telephoneCheck(str) {
-  return true;
-}
+  let regionCodeRegex = /^(1\s?)?/;
+  let areaCodeRegex = /(\(\d{3}\)|\d{3})/;
+  let spacingRegex = /[\s-]?/;
+  let threeNumbersRegex = /\d{3}/;
+  let foursNumbersRegex = /\d{4}$/;
+  let fullRegex = new RegExp(regionCodeRegex.source + areaCodeRegex.source + spacingRegex.source + threeNumbersRegex.source + spacingRegex.source + foursNumbersRegex.source);
 
-telephoneCheck("555-555-5555");
-
-
-function isValidCountryCodeAndSize(str) {
-  let newStr = str.replaceAll('-', '')
-  newStr = newStr.replaceAll(' ', '')
-  if (newStr.length === 11) {
-  if (newStr.charAt(0) != '1'||newStr.charAt(1)!='('||typeof newStr.chartAt(1)== Number ) {
-  return false;
-}
-    else {
-      return true;
-    }
-  }
-  else if (newStr.length > 11 || newStr.length < 10) {
-    return false;
-  }
-  else {
-    return true;
-  }
-}
-
-function parenthesesChecker(str) {
-  let newStr = str.replaceAll('-', '');
-  newStr = newStr.replaceAll(' ', '');
-  if (newStr.charAt(0) === '(' && newStr.charAt(4) === ')') {
-    return true;
-  }
-  else if (newStr.charAt(1) === '(' && newStr.charAt(5) == ')') {
-    return true;
-  }
-  else {
-    return false
-  }
-}
-function verifySpacesAndNumbers(str) {
-  let newStr = str.replaceAll('-', '');
-  let phoneSize = newStr.length;
-  for (let i = 0; i < phoneSize; i++) {
-    if (isNaN(newStr.charAt(i))) {
-      return false;
-    }
-  }
-  let spacePattern = "   ";
-  let spaceIndex = newStr.indexOf(spacePattern);
-  
-  if (spaceIndex !== 5 && spaceIndex !== 6) {
-    return false;
-  }
-
-  return true;
+  return fullRegex.test(str);
 }
